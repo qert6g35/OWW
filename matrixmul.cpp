133b895518a661ee47f70391df774274204d6c0c
Piotr Kulczycki <piotr9833@gmail.com>
2024-10-26 20:34:00 +0200

    added data generation and Transition function

--- matrixmul.cpp
+++ matrixmul.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
 #include <fstream>
+#include <random>
 #include <chrono>
 #include <thread>
 #include <vector>
 
-static const int NUMBER_OF_THREADS = 4;
-static const int MATRIX_SIZE = 5;
+static const int NUMBER_OF_THREADS = 2;
+static const int MATRIX_SIZE = 2;
 using type = int;
 
 // using Matrix = std::vector<std::vector<int>>;
+
+void T(type *m){
+    static type mT[MATRIX_SIZE * MATRIX_SIZE] = {0};
+    for (int i = 0; i < MATRIX_SIZE; i++)
+        for (int j = 0; j < MATRIX_SIZE; j++){
+            mT[i * MATRIX_SIZE + j] = m[j * MATRIX_SIZE + i];
+        }
+    for (int i = 0; i < MATRIX_SIZE*MATRIX_SIZE; i++)
+        m[i] = mT[i];
+}
+
+void generate_data(type *m, const type range_from,const type range_to){
+    std::random_device                  rand_dev;
+    std::mt19937                        generator(rand_dev());
+    std::uniform_int_distribution<type>  distr(range_from, range_to);
+    //type sum;
+    for(int i = 0; i<MATRIX_SIZE*MATRIX_SIZE; i++){
+        m[i] = distr(generator);
+        std::cout << m[i] ;
+    }
+    //std::cout << "sum:" << sum << "   we sum n values, n:"<< MATRIX_SIZE*MATRIX_SIZE <<std::endl;
+}
 
 void matmul_single(type *c, const type *a, const type *b) {
     for (int i = 0; i < MATRIX_SIZE; i++)
@@ -51,11 +74,15 @@ void matmul_thread_t(type *c, const type *a, const type *b, int thread_id) {
 
     for(int elem_id = start_op; elem_id < finish_op; elem_id++){
         row_a = (elem_id/MATRIX_SIZE);
-        col_b = (elem_id%MATRIX_SIZE); // zakładam że transponowano b i ze indeksowanie teraz jest z gory na dol
-
+        //      0 0 0  id/size dla size = 3 macierzy wyjściowej
+        //      1 1 1
+        //      2 2 2
         start_a = (row_a*MATRIX_SIZE);
         // finish_a = start_a + MATRIX_SIZE;
-
+        col_b = (elem_id%MATRIX_SIZE); // zakładam że transponowano b i ze indeksowanie teraz jest z gory na dol
+        //      0 1 2  id%size  dla size = 3 macierzy wyjściowej
+        //      0 1 2
+        //      0 1 2
         start_b = (col_b*MATRIX_SIZE); 
         // finish_b = start_b + MATRIX_SIZE;
         elem_of_c = 0;
@@ -84,19 +111,29 @@ void matmul_threaded(type *c, const type *a, const type *b) {
 
 int main() {
     type a[MATRIX_SIZE * MATRIX_SIZE];
-    type b[MATRIX_SIZE * MATRIX_SIZE];
+    type b[MATRIX_SIZE * MATRIX_SIZE]; //=  { 1,0,0,
+    //                                        0,1,0,
+    //                                        0,0,1};
     type c[MATRIX_SIZE * MATRIX_SIZE] = {0};
 
-    for (int i = 0; i < MATRIX_SIZE * MATRIX_SIZE; ++i) {
-        a[i] = i;
-        b[i] = i;
-    }
+    const type range_start = 0;
+    const type range_end = 10;
+
+    generate_data(a,range_start,range_end);
+    generate_data(b,range_start,range_end);
+    //generate_data(b,range_start,range_end);
+
+    // for (int i = 0; i < MATRIX_SIZE * MATRIX_SIZE; ++i) {
+    //     a[i] = i;
+    //     b[i] = i;
+    // }
 
     std::cout << "Matrix A:" << std::endl;
     showMatrix(a);
+    T(b);
     std::cout << "Matrix B:" << std::endl;
     showMatrix(b);
-
+    T(b);
     matmul_threaded(c, a, b);
     std::cout << "Result Matrix C:" << std::endl;
     showMatrix(c);
