baf3424bf1d1a3b0d62866068ab5febf148832f4
Piotr Kulczycki <piotr9833@gmail.com>
2024-10-26 14:32:12 +0200

    help me

--- matrixmul.cpp
+++ matrixmul.cpp
@@ -1,24 +1,55 @@
 #include <iostream>
 #include <fstream>
+#include <chrono>
+#include <thread>
 
-void matmul(const float *a, const float *b, float *c, int n) {
+static const int NUMBER_OF_THREADS = 2;
+static const int n = 2;
+typedef float type;
+
+void matmul(const type *a, const type *b, type *c) {
     for (int i = 0; i < n; i++)
         for (int j = 0; j < n; j++)
             for (int k = 0; k < n; k++)
                 c[i * n + j] += a[i * n + k] * b[k * n + j];
 }
 
-int main(){
-    float a[2][2] = {
+void showMatrix(const type *matrix){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            std::cout<<matrix[i * n + j]<<" ";
+        }
+        std::cout<< std::endl;
+    }
+}
+
+void runThreds(){
+    type a[2][2] = {
                     { 1, 2 },
                     { 3, 4 }
                     };
-    float b[2][2] = {
-                { 1, 2 },
-                { 3, 4 }
+    type b[2][2] = {
+                { 1, 0 },
+                { 0, 1 }
                 };
-    float c[2][2];
-    matmul(*a, *b, *c, 2);
-    std::cout << c[1][1] << std::endl;
+    type c[2][2];
+
+    std::thread Threads[NUMBER_OF_THREADS];
+
+    for(int i = 0; i<NUMBER_OF_THREADS; i++){
+        Threads[i] = std::thread(matmul,*a,*b,*c);
+    }
+
+    for(int i = 0; i<NUMBER_OF_THREADS; i++){
+        Threads[i].join();
+    }
+
+    showMatrix(*c);
+
+}
+
+int main(){
+    runThreds();
     return 0;
 }
+
