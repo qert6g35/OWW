71cf87d2adc0cf740c4a692444e6f50c31d8e55e
szolkiewicz <s.d.zolkiewicz@gmail.com>
2024-10-26 18:49:47 +0200

    coś tam działa

--- matrixmul.cpp
+++ matrixmul.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 
 static const int NUMBER_OF_THREADS = 4;
-static const int MATRIX_SIZE = 2;
+static const int MATRIX_SIZE = 5;
 using type = int;
 
 // using Matrix = std::vector<std::vector<int>>;
@@ -27,16 +27,53 @@ void showMatrix(const type *matrix){
 }
 
 
-// // Funkcja mnożąca fragment macierzy
-void matmul_thread(type *c, const type *a, const type *b, int thread_id) {
+// zakładam że ktos transponował B i teraz indeksy idą tak
+// B =  0 3 6
+//      1 4 7 
+//      2 5 8
+// 
+// A =  0 1 2 
+//      3 4 5
+//      6 7 8
 
+void matmul_thread_t(type *c, const type *a, const type *b, int thread_id) {
+    int all_operations = (MATRIX_SIZE * MATRIX_SIZE);
+    int op_for_one = all_operations/NUMBER_OF_THREADS;
+    int rest_op = all_operations%NUMBER_OF_THREADS;
+
+    int start_op = op_for_one * thread_id + rest_op * ( thread_id != 0 );
+    int finish_op = op_for_one * (thread_id+1) + rest_op + rest_op;
+
+    type elem_of_a, elem_of_b;
+    type elem_of_c;
+    int row_a, start_a, finish_a;
+    int col_b, start_b, finish_b;
+
+    for(int elem_id = start_op; elem_id < finish_op; elem_id++){
+        row_a = (elem_id/MATRIX_SIZE);
+        col_b = (elem_id%MATRIX_SIZE); // zakładam że transponowano b i ze indeksowanie teraz jest z gory na dol
+
+        start_a = (row_a*MATRIX_SIZE);
+        // finish_a = start_a + MATRIX_SIZE;
+
+        start_b = (col_b*MATRIX_SIZE); 
+        // finish_b = start_b + MATRIX_SIZE;
+        elem_of_c = 0;
+        for(int i = 0; i < MATRIX_SIZE; i++){
+            elem_of_a = start_a + i;
+            elem_of_b = start_b + i;
+            elem_of_c += elem_of_a * elem_of_b;
+        }
+        c[elem_id] = elem_of_c;
+        
+    }
 }
 
 // Funkcja inicjująca wątki
 void matmul_threaded(type *c, const type *a, const type *b) {
     std::thread threads[NUMBER_OF_THREADS];
     for (int i = 0; i < NUMBER_OF_THREADS; ++i) {
-        threads[i] = std::thread(matmul_thread, c, a, b, i);
+        threads[i] = std::thread(matmul_thread_t, c, a, b, i);
     }
 
     for (int i = 0; i < NUMBER_OF_THREADS; ++i) {
@@ -51,8 +88,8 @@ int main() {
     type c[MATRIX_SIZE * MATRIX_SIZE] = {0};
 
     for (int i = 0; i < MATRIX_SIZE * MATRIX_SIZE; ++i) {
-        a[i] = rand() % 10;
-        b[i] = rand() % 10;
+        a[i] = i;
+        b[i] = i;
     }
 
     std::cout << "Matrix A:" << std::endl;
